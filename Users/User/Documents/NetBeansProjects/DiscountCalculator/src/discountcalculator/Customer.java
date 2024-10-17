f814c00fd8e8f5f3d39e92b6cc2f0676fa4f4635
Pavel <iverick091@gmail.com>
2024-10-17 21:21:46 +0100

    Modified code to read data from an input file and store it in a Customer object.

--- Users/User/Documents/NetBeansProjects/DiscountCalculator/src/discountcalculator/Customer.java
+++ Users/User/Documents/NetBeansProjects/DiscountCalculator/src/discountcalculator/Customer.java
@@ -11,9 +11,45 @@ package discountcalculator;
 public class Customer {
     // List of Customer's properties
     private String name;
-    private int year;
+    private int lastPurchaseYear;
     private int customerClass;
     private double baseValue;
     private double discount;
     private double finalValue;
+    
+    // Construcor for the Person object.
+    // Sets object properties based on the data we read from the input file.
+    public Customer(String name, double baseValue, int customerClass, int lastPurchaseYear) {
+        this.name = name;
+        this.baseValue = baseValue;
+        this.customerClass = customerClass;
+        this.lastPurchaseYear = lastPurchaseYear;
+
+        calculateDiscount();
+        calculateFinalValue();
+    }
+    
+    // This method calculates the discout based on the year of the last purchase and customer class.
+    // TODO: Implement function to calculate proper discount.
+    private void calculateDiscount() {
+        this.discount = 0.9;
+    }
+    
+    // This method calculates the final value based on the base value and the customer class.
+    private void calculateFinalValue() {
+        this.finalValue = this.baseValue * this.discount;
+    }
+    
+    // Getters methods
+    public String getName() {
+        return this.name;
+    }
+    
+    public double getBaseValue() {
+        return this.baseValue;
+    }
+    
+    public double getFinalValue() {
+        return this.finalValue;
+    }
 }
--- Users/User/Documents/NetBeansProjects/DiscountCalculator/src/discountcalculator/DiscountCalculator.java
+++ Users/User/Documents/NetBeansProjects/DiscountCalculator/src/discountcalculator/DiscountCalculator.java
@@ -21,16 +21,41 @@ public class DiscountCalculator {
         // Makes it easier to change them later.
         String inputFilePath = "C:\\Users\\User\\Documents\\NetBeansProjects\\DiscountCalculator\\src\\discountcalculator\\customers.txt";
         String outputFilePath = "TODO: Enter here";
-        
+
         // This block of code reads the data from an input file
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFilePath))) {
             String userName;
+
+            // Use while block to read the data from the file until we reach an end of the file
             while ((userName = bufferedReader.readLine()) != null) {
-                System.out.println(userName);
+                String baseValueString = bufferedReader.readLine();
+                String customerClassString = bufferedReader.readLine();
+                String lastPurchaseYearString = bufferedReader.readLine();
+
+                // The following block converts string variables into a data
+                // that can be assigned to properties of Customer object.
+                // Use try/catch block here to perform validation checks.
+                // 
+                // TODO: Implement validations.
+                try {
+                    double baseValue = Double.parseDouble(baseValueString);
+                    int customerClass = Integer.parseInt(customerClassString);
+                    int lastPurchaseYear = Integer.parseInt(lastPurchaseYearString);
+                    
+                    // Create a new customer with the data read from the input file.
+                    Customer customer = new Customer(userName, baseValue, customerClass, lastPurchaseYear);
+                
+                    System.out.println(customer.getName());
+                    System.out.println(customer.getBaseValue());
+                    System.out.println(customer.getFinalValue());
+                } catch (Exception e) {
+                    System.err.println(e);
+                }
             }
-        } catch (Exception e ) {
+        } catch (Exception e) {
             System.err.println(e);
         }
     }
+
     
 }
